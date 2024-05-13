9b7a404556827af0541049b23364c8ff34371709
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-13 19:58:08 +0100

    script(Readme): script that reads and prints the content of a file.

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs')
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    // If an error occurred, print the error object
+    console.error(err)
+  } else {
+    console.log(data.toString())
+  }
+})
