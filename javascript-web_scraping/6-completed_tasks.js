0c843989cdee89ee39c35a35447baf418759202d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-15 22:45:39 +0100

    script(request):script that computes the number of tasks completed by user id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,20 +4,20 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching the API:', error);
-    return;
-  }
-
-  let todos = JSON.parse(body);
-  const completedTasksPerUserId = {};
-  todos.forEach(todo => {
-    if (todo.completed && !completedTasksPerUserId[todo.userId]) {
-      completedTasksPerUserId[todo.userId] = 1;
-    } else {
-      completedTasksPerUserId[todo.userId]++;
+    if (error) {
+        console.error('Error fetching the API:', error);
+        return;
     }
-  });
-  console.log(completedTasksPerUserId);
+
+    let todos = JSON.parse(body);
+    const completedTasksPerUserId = {};
+    todos.forEach(todo => {
+        if (todo.completed && !completedTasksPerUserId[todo.userId]) {
+            completedTasksPerUserId[todo.userId] = 1;
+        } else {
+            completedTasksPerUserId[todo.userId]++;
+        }
+    });
+    console.log(completedTasksPerUserId);
 
 });
