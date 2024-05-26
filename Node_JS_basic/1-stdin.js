f62d34dad6182a245abe21650652d47824cbaf42
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-05-26 19:29:46 +0100

    NodeJS Basics

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,11 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin
 });
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-rl.question('', (name) => {
-  process.stdout.write(`Your name is: ${name}\n`);
-  rl.close();
- process.stdout.write("This important software is now closing\n");
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
+process.stdout.write("This important software is now closing\n");
